8983024904204702ac6f0c0605107bbd2be3c685
ThanhVinh2712 <you@example.com>
2021-09-22 19:20:34 +0700

    branch master

---
+++ src/main/java/com/example/spring_boot_jwt_lntv/SpringBootJwtLntvApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_lntv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtLntvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtLntvApplication.class, args);
+    }
+
+}
